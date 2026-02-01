367ca511a25e835120d74157f80094fb574285f0
KimAwk <kimanh57682016@gmail.com>
2026-02-01 19:07:56 +0700

    first-commit

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,25 @@
+const MyConstants = {
+  DB_SERVER: 'banhang.otdtmk7.mongodb.net',
+  DB_USER: 'kimanh',
+  DB_PASS: 'VLU19122005',
+  DB_DATABASE: 'shoppingonline',
+  EMAIL_USER: 'kimanh57682016@gmail.com', // Microsoft mail service
+  EMAIL_PASS: '113113113Ka',
+  JWT_SECRET: 'f4148f965ed97685dcfe2e676cdb54a4a5bc62b8b8ac44afecfe3c7746c4866d9230694d3bd6373558a5f52aa2fe1edaa0c123bdb1786c6bc4f2a3a07390b2db',
+  JWT_EXPIRES: '36000', // in milliseconds
+};
+module.exports = MyConstants;
+
+// const MyConstants = {
+//   DB_SERVER: 'banhang.otdtmk7.mongodb.net',
+//   DB_USER: 'kimanh',
+//   DB_PASS: 'VLU19122005',
+//   DB_DATABASE: 'shoppingonline',
+//   EMAIL_USER: 'kimanh57682016@gmail.com', // Microsoft mail service
+//   EMAIL_PASS: '113113113Ka',
+//   JWT_SECRET: 'a2cff62edeb577cb9ee3fe5f310bb7a3bd94235e43cf3a48d4ee4afb6a1ee0a65caa8f9523c7d4db751a226589153ff7138bce30cd55d148e1e869117591b08c',
+//   JWT_EXPIRES: '<jwt_expires>', // in milliseconds
+// };
+// module.exports = MyConstants;
+
+//mongodb+srv://kimanh:VLU19122005@banhang.otdtmk7.mongodb.net/
